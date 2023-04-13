0ac969ed1f6544d24b7ba7045ab244303ab806a8
clarkhao <clarktotoro@gmail.com>
2023-04-13 16:11:46 +0800

    init

---
+++ src/main/kotlin/com/clark/totoro/assets/AssetsApplication.kt
@@ -0,0 +1,11 @@
+package com.clark.totoro.assets
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AssetsApplication
+
+fun main(args: Array<String>) {
+	runApplication<AssetsApplication>(*args)
+}
